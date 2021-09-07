f3772bf24f23b1cc4c6ab1edf03a90045d1e09d5
BuJi <buji@iiisland.com>
2021-09-07 15:11:23 +0800

    first commit

---
+++ cli/cli.go
@@ -0,0 +1,134 @@
+package cli
+
+import (
+	"bufio"
+	"errors"
+	"io"
+	"strconv"
+	"strings"
+)
+
+const CHAR_SPACE = ' '
+
+const CR = '\r'
+const LF = '\n'
+const CRLF = "\r\n"
+
+const SimpleString = '+'
+const Err = '-'
+const Number = ':'
+const Array = '*'
+const String = '$'
+
+var Nil = errors.New("nil")
+
+func GenByte(command []byte) [][]byte {
+	args := make([][]byte, 0)
+
+	escape := false
+	argsIndex := 0
+
+	args = append(args, []byte{})
+	for i, l := 0, len(command); i < l; i++ {
+		b := command[i]
+		if b == CHAR_SPACE && !escape {
+			argsIndex++
+			args = append(args, []byte{})
+			escape = false
+		} else {
+			if b == '"' || b == '\'' {
+				escape = true
+			}
+			args[argsIndex] = append(args[argsIndex], b)
+		}
+	}
+	return args
+}
+
+func Convert2Command(bs []byte) []byte {
+	c := GenByte(bs)
+
+	b := make([]byte, 0)
+	b = append(b, '*')
+
+	size := len(c)
+	b = append(b, []byte(strconv.Itoa(size))...)
+	b = append(b, '\r', '\n')
+
+	for _, i2 := range c {
+		b = append(b, '$')
+		size := len(i2)
+		b = append(b, []byte(strconv.Itoa(size))...)
+		b = append(b, '\r', '\n')
+		b = append(b, i2...)
+		b = append(b, '\r', '\n')
+	}
+	return b
+}
+
+func Resp(reader io.Reader) (interface{}, error) {
+	read := bufio.NewReader(reader)
+	r, _ := read.ReadString(LF)
+	result, _ := _resp(r, read)
+	return result, nil
+}
+
+func _simpleString(head string, reader *bufio.Reader) (string, error) {
+	strLen := len(head) - 2
+	return head[1:strLen], nil
+}
+
+func _err(head string, reader *bufio.Reader) (string, error) {
+	strLen := len(head) - 2
+	return head[1:strLen], nil
+}
+
+func _string(head string, reader *bufio.Reader) (string, error) {
+	s := strings.TrimSpace(head[1:])
+	size, _ := strconv.Atoi(s)
+	if size <= -1 {
+		return "", Nil
+	}
+	b := make([]byte, size+2)
+	reader.Read(b)
+	return string(b[:size]), nil
+}
+
+func _number(head string, reader *bufio.Reader) (int, error) {
+	s := strings.TrimSpace(head[1:])
+	size, _ := strconv.Atoi(s)
+	return size, nil
+}
+
+func _array(head string, reader *bufio.Reader) ([]interface{}, error) {
+	s := strings.TrimSpace(head[1:])
+	size, _ := strconv.Atoi(s)
+	if size <= -1 {
+		return nil, Nil
+	}
+	r := make([]interface{}, 0, size)
+	for i := 0; i < size; i++ {
+		s, _ := reader.ReadString(LF)
+		resp, _ := _resp(s, reader)
+		r = append(r, resp)
+	}
+	return r, nil
+
+}
+
+func _resp(head string, reader *bufio.Reader) (interface{}, error) {
+	c := head[0]
+	switch c {
+	case SimpleString:
+		return _simpleString(head, reader)
+	case Err:
+		return _err(head, reader)
+	case Number:
+		return _number(head, reader)
+	case Array:
+		return _array(head, reader)
+	case String:
+		return _string(head, reader)
+	}
+	return nil, errors.New("unsupport operators")
+}
---
+++ cmd/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"bufio"
+	"encoding/json"
+	"github.com/tangtj/gorediscli/cli"
+	"log"
+	"net"
+	"os"
+)
+
+func main() {
+	c, err := net.Dial("tcp", "127.0.0.1:6379")
+	if err != nil {
+		log.Println(err)
+	}
+	reader := bufio.NewReader(os.Stdin)
+	for {
+		print("> ")
+		line, _, _ := reader.ReadLine()
+		line = cli.Convert2Command(line)
+		c.Write(line)
+		r, _ := cli.Resp(c)
+		str, _ := json.Marshal(r)
+		println(string(str))
+	}
+}
